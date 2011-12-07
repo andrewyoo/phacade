7825828f44412c87ae4416fb96516250c186beeb
Andrew Yoo <andrew@flywheelnetworks.com>
2011-12-06 18:08:23 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phacade::Application.config.secret_token = '5eef4ecd6cfa3357a80a4376c37562b168e1edbb12a23a90227101f37aed3c7c628320d77dae02ea5cb32c5e584b0f886804fc166c3c242e1b46d5b355b798d6'
