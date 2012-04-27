9366628d338d6c24f491862b21ecdb63bb536b38
Andrew Yoo <andrew@flywheelnetworks.com>
2012-04-27 12:17:51 -0700

    updated titles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,26 @@
 class HomeController < ApplicationController
   def index
+    @title = "Welcome To Phacade"
   end
 
   def about
+    @title = "About Me"
     render :action => :index
   end
 
   def projects
+    @title = "My Projects"
   end
 
   def blogs
+    @title = "My Blogs"
   end
 
   def contact
+    @title = "Contact Me"
   end
 
   def resume
+    @title = "My Resume"
   end
 end
